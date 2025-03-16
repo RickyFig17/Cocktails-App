f8d40db0d0f2cb748a5374bc593034bbc3005ecb
ricardo <rfigueroa2317@gmail.com>
2025-03-16 17:32:56 -0400

    finishing 2oz array list

--- src/TwoOzCocktails.js
+++ src/TwoOzCocktails.js
@@ -13,9 +13,111 @@ function TwoOzCocktailsList() {
   const cocktails = [
     {
       name: "Black Russian",
+      liquor1: "1oz Vodka",
+      liquor2: "1oz Kahlua",
+      filler: "",
       glass: "Rocks Glass",
       mixingMethod: "Build on Ice",
-      garnish: "N/A",
+      garnish: "none",
+    },
+    {
+      name: "White Russian",
+      liquor1: "1oz Vodka",
+      liquor2: "1oz Kahlua",
+      filler: "Splash Of Cream",
+      glass: "Rocks Glass",
+      mixingMethod: "Build on Ice",
+      garnish: "none",
+    },
+    {
+      name: "Kahlua And Cream",
+      liquor1: "2oz Kahlua",
+      liquor2: "N/A",
+      filler: "Splash Of Cream",
+      glass: "Rocks Glass",
+      mixingMethod: "Build on Ice",
+      garnish: "none",
+    },
+    {
+      name: "Toasted Almond",
+      liquor1: "1oz Amaretto",
+      liquor2: "1oz Kahlua",
+      filler: "Splash of Cream",
+      glass: "Rocks Glass",
+      mixingMethod: "Build on Ice",
+      garnish: "none",
+    },
+    {
+      name: "Gimlet",
+      liquor1: "2oz Gin",
+      liquor2: "N/A",
+      filler: "Splash of Rose's Lime Juice",
+      glass: "Rocks Glass",
+      mixingMethod: "Build on Ice",
+      garnish: "Lime Wedge",
+    },
+    {
+      name: "Vodka Gimlet",
+      liquor1: "2oz Vodka",
+      liquor2: "N/A",
+      filler: "Splash of Roses's Lime Juice",
+      glass: "Rocks Glass",
+      mixingMethod: "Build on Ice",
+      garnish: "Lime Wedge",
+    },
+    {
+      name: "God Father",
+      liquor1: "1oz Scotch",
+      liquor2: "1oz Amaretto",
+      filler: "N/A",
+      glass: "Rocks Glass",
+      mixingMethod: "Build on Ice",
+      garnish: "Orange",
+    },
+    {
+      name: "God Mother",
+      liquor1: "1oz Vodka",
+      liquor2: "1oz Amaretto",
+      filler: "N/A",
+      glass: "Rocks Glass",
+      mixingMethod: "Build on Ice",
+      garnish: "Lime Wedge",
+    },
+    {
+      name: "Stinger",
+      liquor1: "1oz Brandy",
+      liquor2: "1oz White Creme de Menthe",
+      filler: "N/A",
+      glass: "Rocks Glass",
+      mixingMethod: "Build on Ice",
+      garnish: "Lemon or Mint",
+    },
+    {
+      name: "Colorado Bulldog",
+      liquor1: "1oz Vodka",
+      liquor2: "1oz Kahlua",
+      filler: "Splash of Coke and Cream",
+      glass: "Rocks Glass",
+      mixingMethod: "Build on Ice",
+      garnish: "none",
+    },
+    {
+      name: "Kamikaze",
+      liquor1: "1oz Vodka",
+      liquor2: "1oz Triple Sec",
+      filler: "Splash of Rose's Lime Juice",
+      glass: "Rocks Glass",
+      mixingMethod: "Build on Ice",
+      garnish: "Lime Wedge",
+    },
+    {
+      name: "Rusty Nail",
+      liquor1: "1oz Scotch",
+      liquor2: "1oz Drambuie",
+      filler: "none",
+      glass: "Rocks Glass",
+      mixingMethod: "Build on Ice",
+      garnish: "Lemon Peel",
     },
   ];
 
@@ -24,6 +126,9 @@ function TwoOzCocktailsList() {
       {cocktails.map((cocktail, index) => (
         <li key={index}>
           <h3>{cocktail.name}</h3>
+          <h4>Alcohol 1: {cocktail.liquor1}</h4>
+          <h4>Alcohol 2: {cocktail.liquor2}</h4>
+          <h4>{cocktail.filler}</h4>
           <p>{cocktail.glass}</p>
           <p>{cocktail.mixingMethod}</p>
           <p>{cocktail.garnish}</p>
