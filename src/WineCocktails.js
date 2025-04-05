0c281ea8febf71825d681b187b61a78a627332dd
ricardo <rfigueroa2317@gmail.com>
2025-04-05 11:36:04 -0400

    adding scss to wine cocktails

--- src/WineCocktails.js
+++ src/WineCocktails.js
@@ -9,6 +9,25 @@ function WineCocktails() {
   );
 }
 
+function CocktailCard({ cocktail }) {
+  return (
+    <div className="cocktail-card">
+      <h3>{cocktail.name}</h3>
+      <p>
+        <strong>{cocktail.alcohol1}</strong>
+      </p>
+      <p>
+        <strong>{cocktail.alcohol2}</strong>
+      </p>
+      <p>{cocktail.filler1}</p>
+      <p>{cocktail.filler2}</p>
+      <p>{cocktail.glass}</p>
+      <p>{cocktail.mixingMethod}</p>
+      <p>{cocktail.garnish}</p>
+    </div>
+  );
+}
+
 function WineCocktailList() {
   const cocktails = [
     {
@@ -32,21 +51,13 @@ function WineCocktailList() {
       garnish: "none",
     },
   ];
+
   return (
-    <ul>
+    <div className="cocktail-list">
       {cocktails.map((cocktail, index) => (
-        <li key={index}>
-          <h3>{cocktail.name}</h3>
-          <h4>{cocktail.alcohol1}</h4>
-          <h4>{cocktail.alcohol2}</h4>
-          <h4>{cocktail.filler1}</h4>
-          <h4>{cocktail.filler2}</h4>
-          <p>{cocktail.glass}</p>
-          <p>{cocktail.mixingMethod}</p>
-          <p>{cocktail.garnish}</p>
-        </li>
+        <CocktailCard key={index} cocktail={cocktail} />
       ))}
-    </ul>
+    </div>
   );
 }
 
